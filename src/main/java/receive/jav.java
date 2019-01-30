2e20a045b8a70908643f3291c19f03caba63ae50
training <training@trainings-Mac-mini-3.local>
2019-01-30 15:32:01 -0600

    Receive

---
+++ src/main/java/receive/jav.java
@@ -0,0 +1,33 @@
+package receive;
+
+//RECEIVE
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.DeliverCallback;
+
+public class jav {
+
+
+    private static final String TASK_QUEUE_NAME = "task_queue";
+
+    public static void main(String[] argv) throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        final Connection connection = factory.newConnection();
+        final Channel channel = connection.createChannel();
+
+        channel.queueDeclare(TASK_QUEUE_NAME, true, false, false, null);
+
+       // channel.basicQos(1);
+
+        DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+            String message = new String(delivery.getBody(), "UTF-8");
+            System.out.println(" [x] Received '" + message + "'");
+        };
+        channel.basicConsume(TASK_QUEUE_NAME, true, deliverCallback, consumerTag -> { });
+
+    }
+
+}
